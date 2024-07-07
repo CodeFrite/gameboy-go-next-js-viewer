93a501c4b521af15c9bf596980da423d12741a15
codeFrite <codefrite@gmail.com>
2024-07-07 09:33:48 +0200

    value-watcher is highlighted for 1 second when the value changes

--- components/command-line/value-watcher.tsx
+++ components/command-line/value-watcher.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Uint16, Uint8 } from "../types";
+import styles from "./index.module.css";
 
 // Value Watcher: Display the value of a register or memory address and change its color based on the changes
 export type ValueWatcherProps = {
@@ -9,15 +10,22 @@ export type ValueWatcherProps = {
 
 const ValueWatcher = (props: ValueWatcherProps) => {
   const [state, setState] = useState(props.value);
+  const [changed, setChanged] = useState(false);
 
   useEffect(() => {
     setState(props.value);
-    // play css animation$
-    const watcher = document.querySelector(".value-watcher");
-    watcher?.classList.add("changed");
+    setChanged(true);
+    const timeout = setTimeout(() => {
+      setChanged(false);
+    }, 500);
+    return () => clearTimeout(timeout);
   }, [props.value]);
 
-  return <span className="value-watcher">{props.value?.toString()}</span>;
+  return (
+    <span className={!changed ? styles.value_watcher : styles.value_watcher_changed}>
+      {props.value?.toString()}
+    </span>
+  );
 };
 
 export default ValueWatcher;
