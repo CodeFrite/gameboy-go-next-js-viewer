3faa009ec371befffe2f44e9501a63f57d59d29e
codeFrite <codefrite@gmail.com>
2024-07-07 23:58:22 +0200

    add a header with last byte of address to the Memory component

--- components/memory/index.tsx
+++ components/memory/index.tsx
@@ -27,19 +27,46 @@ const Memory = (props: MemoryProps) => {
   };
 
   const printMemory = () => {
-    console.log(props.data.length);
-
     return props.data.map((value, index) => {
       let result = [];
       // Highlight the PC
 
       if (index % 16 === 0) {
+        // add memory table header
+        if (index == 0) {
+          result.push(<br />);
+          result.push(<span className={styles.hex_cell + " " + styles.header}>ADDR </span>);
+          result.push(
+            [
+              "X0",
+              "X1",
+              "X2",
+              "X3",
+              "X4",
+              "X5",
+              "X6",
+              "X7",
+              "X8",
+              "X9",
+              "XA",
+              "XB",
+              "XC",
+              "XD",
+              "XE",
+              "XF",
+            ].map((value) => (
+              <span key={index + "-header"} className={styles.hex_cell + " " + styles.header}>
+                {value}
+              </span>
+            ))
+          );
+        }
         // Add a line break every 16 cells and add the line address
         result.push(<br />);
         result.push(
-          <span key={index + "-line-address"} className={styles.hex_address}>
+          <span key={index + "-line-address"} className={styles.address}>
             {(props.address + index).toString(16).padStart(4, "0").toUpperCase().substring(0, 3) +
-              "X"}
+              "X "}
           </span>
         );
       }
