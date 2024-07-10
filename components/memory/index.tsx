6c7f67a5609e1c6307cfa2d71bbf9ef112b95691
codeFrite <codefrite@gmail.com>
2024-07-10 15:16:14 +0200

    refactored the memory component in order to simplify the code and prepare the code before developing the new feature which will allow a memory to zoom to a viewport that is centered around the program counter

--- components/memory/index.tsx
+++ components/memory/index.tsx
@@ -1,4 +1,6 @@
 import styles from "./index.module.css";
+import gStyles from "../../styles/globals.module.css";
+import { BrandColor } from "../command-line/types";
 
 export type MemoryWriter = {
   name: string;
@@ -12,69 +14,95 @@ export type MemoryProps = MemoryWriter & {
 };
 
 const Memory = (props: MemoryProps) => {
-  const pc = props.pc - props.address;
-  const formatMemoryCell = (value: string, index: number): string => {
-    let style = styles.hex_cell;
-    // pc is black/yellow
-    if (index === pc) {
-      style += " " + styles.pc;
-    }
-    // operands are black/light-blue
-    else if (index > pc && index < pc + props.bytes) {
-      style += " " + styles.operand;
-    }
-    // other memory cells are light-blue/black
-    return style;
+  const printMemoryCell = (index: number): JSX.Element => {
+    const value: string = props.data[index].toString();
+
+    return (
+      <span key={"cell-" + props.name + "-" + index} className={styles.hex_cell}>
+        {value.substring(2)}
+      </span>
+    );
   };
 
-  const printMemory = () => {
-    return props.data.map((value, memoryIndex) => {
-      if (memoryIndex >= 16 * 16) return;
-      let result = [];
-      // Highlight the PC
+  const printPC = (index: number): JSX.Element => {
+    return (
+      <span key={"cell-" + props.name + "-" + index} className={styles.hex_cell + " " + styles.pc}>
+        {props.data[index].substring(2)}
+      </span>
+    );
+  };
 
-      if (memoryIndex % 16 === 0) {
-        // add memory table header
-        if (memoryIndex == 0) {
-          result.push(
-            <span
-              key={"table-label-" + props.name + "-" + memoryIndex}
-              className={`${styles.hex_cell} ${styles.header}`}>
-              ADDR{" "}
-            </span>
-          );
-          result.push(
-            ...Array.from({ length: 16 }, (_, index) => (
-              <span
-                key={"table-header-" + props.name + "-" + index}
-                className={`${styles.hex_cell} ${styles.header}`}>
-                X{index.toString(16).toUpperCase()}
-              </span>
-            ))
-          );
+  const printOperand = (index: number): JSX.Element => {
+    return (
+      <span
+        key={"cell-" + props.name + "-" + index}
+        className={styles.hex_cell + " " + styles.operand}>
+        {props.data[index].substring(2)}
+      </span>
+    );
+  };
+  const printMemoryLine = (line: number): JSX.Element[] => {
+    let result: JSX.Element[] = [];
+    // print line break
+    result.push(<br key={"line-break-" + props.name + "-" + line} />);
+    // print line memory address
+    result.push(
+      <span
+        key={"line-address-" + props.name + "-" + line}
+        className={styles.hex_cell + " " + gStyles.color_yellow}>
+        {(props.address + line * 16).toString(16).padStart(4, "0").substring(0, 3) + "X"}
+      </span>
+    );
+    // print the 16 memory cells of the current line
+    result.push(
+      ...props.data.slice(line * 16, (line + 1) * 16).map((value, memoryIndex) => {
+        // highlight the program counter
+        if (props.pc === line * 16 + memoryIndex) {
+          return printPC(line * 16 + memoryIndex);
+          // highlight the operands
+        } else if (props.pc >= props.address + line * 16 + memoryIndex) {
+          return printOperand(line * 16 + memoryIndex);
         }
-        // Add a line break every 16 cells and add the line address
-        result.push(<br key={props.name + "-" + memoryIndex} />);
-        result.push(
-          <span key={"line-header-" + props.name + "-" + memoryIndex} className={styles.address}>
-            {(props.address + memoryIndex)
-              .toString(16)
-              .padStart(4, "0")
-              .toUpperCase()
-              .substring(0, 3) + "X "}
-          </span>
-        );
-      }
-      // add the value of the memory cell
-      result.push(
+        return printMemoryCell(line * 16 + memoryIndex);
+      })
+    );
+    return result;
+  };
+
+  const printMemoryTableHeader = (line: number): JSX.Element[] => {
+    // display 'ADDR' and 'X0' to 'XF' as memory table header
+    let result: JSX.Element[] = [];
+    result.push(
+      <span
+        key={"table-label-" + props.name + "-" + line}
+        className={`${styles.hex_cell} ${styles.header}`}>
+        ADDR
+      </span>
+    );
+    result.push(
+      ...Array.from({ length: 16 }, (_, index) => (
         <span
-          key={"cell-" + props.name + "-" + memoryIndex}
-          className={formatMemoryCell(value.substring(2), memoryIndex)}>
-          {value.substring(2)}
+          key={"table-header-" + props.name + "-" + index}
+          className={`${styles.hex_cell} ${styles.header}`}>
+          X{index.toString(16).toUpperCase().substring(0, 3)}
         </span>
-      );
-      return result;
-    });
+      ))
+    );
+    return result;
+  };
+
+  const printMemory = (): JSX.Element[] => {
+    let result: JSX.Element[] = [];
+    // for each line up to line 16
+    for (let line = 0; line < 16; line++) {
+      // add memory table header before rendering the first memory line
+      if (line === 0) {
+        result.push(...printMemoryTableHeader(line));
+      }
+      // print the memory line
+      result.push(...printMemoryLine(line));
+    }
+    return result;
   };
 
   return (
